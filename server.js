e26ddf1c77bdcd5963876111989576a074d8fbeb
sourav prasad sah <souravprasadsah1234@gmail.com>
2022-06-28 22:25:45 +0530

    small changes

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const mongoose = require('mongoose')
 const app = require('./app')
 const dotenv = require('dotenv')
 
+
+process.on('uncaughtException', err => {
+    console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+    console.log(err.name, err.message);
+    process.exit(1);
+  });
+
+
+
 dotenv.config({
     path:'./config.env'
 })
@@ -18,3 +27,19 @@ console.log(`Datbase connection successfull...`)
 app.listen(port,()=>{
     console.log(`Listining to port...`)
 })
+
+process.on('unhandledRejection', err => {
+    console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+    console.log(err.name, err.message);
+    server.close(() => {
+      process.exit(1);
+    });
+  });
+  
+  process.on('SIGTERM', () => {
+    console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
+    server.close(() => {
+      console.log('💥 Process terminated!');
+    });
+  });
+  
