5d6a80f03bebbca99e77f4ac9d5a234d62b7bc65
sourav prasad sah <souravprasadsah1234@gmail.com>
2022-06-29 23:20:21 +0530

    sourav/smallchanges

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,7 +8,7 @@ const Email = require('./../utils/email');
 
 const signToken = id => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN
+    expiresIn: process.env.JWT_EXPIRES
   });
 };
 
@@ -17,7 +17,7 @@ const createSendToken = (user, statusCode, req, res) => {
 
   res.cookie('jwt', token, {
     expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+      Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
     secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
